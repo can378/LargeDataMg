9b8da6baa5723b4ab17899128a08f445cd6af5f1
yunji <yunji378mm@naver.com>
2025-03-24 16:34:18 +0900

    fix: check all parquet columns
    parquet파일 모두 싹다 출력

--- checkParquet.py
+++ checkParquet.py
@@ -1,10 +1,18 @@
 import pandas as pd
 
-df = pd.read_parquet("output.parquet")#확인하고자 하는 parquet 파일 추가
+# 출력 옵션 설정
+pd.set_option('display.max_rows', None)  #모든 행 출력
+pd.set_option('display.max_columns', None)  #모든 열 출력
+pd.set_option('display.width', None)  #자동 줄바꿈 방지
+pd.set_option('display.max_colwidth', None)  #컬럼 내용 최대 길이 제한 해제
+
+
+df = pd.read_parquet("output.parquet")#확인하고자 하는 parquet 파일
 
 # 컬럼명에서 공백 제거
 df.columns = df.columns.str.strip()
-print("컬럼 확인:", df.columns)
+print("컬럼:", df.columns)
 
-# 특정 컬럼 값 확인
-print(df[['Column5']].dropna())
+
+#print(df[['Column5']].dropna())
+print(df);
