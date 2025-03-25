97ce0a90a941f9f1952da6299ec5f6dbdc1cb768
yunji <yunji378mm@naver.com>
2025-03-25 10:43:41 +0900

    fix: create parquet file
    create parquet file from txt data

--- saveParquet.py
+++ saveParquet.py
@@ -4,19 +4,36 @@ import pyarrow.parquet as pq
 
 file_path = "./20250313_대용량 데이터 전송 테스트.txt"
 
-# 모든 컬럼을 string으로 처리
-df = pd.read_csv(file_path, sep="|", dtype=str)
+# 모든 컬럼을 string으로 읽기
+df = pd.read_csv(file_path, sep="|", skiprows=[1],dtype=str)
 
-# unnamed 컬럼 제거
+# Unnamed 컬럼 제거
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
-df["seq"] = pd.to_numeric(df["seq"], errors="coerce")
-df["Column2"] = pd.to_numeric(df["Column2"], errors="coerce")
-df["Column3"] = pd.to_numeric(df["Column3"], errors="coerce")
-
-# 컬럼명에서 공백 제거
+# 컬럼명 공백 제거
 df.columns = df.columns.str.strip()
 
+# , 제거 및 숫자 확인. 알맞게 변환
+for col in ["seq", "Column2", "Column3"]:
+    df[col] = df[col].str.strip().str.replace(",", "")  # 공백 & 콤마 제거
+
+    # 숫자 아닌 값들 출력해보기
+    # non_numeric_seq = df[~df["seq"].str.match(r"^\d+$", na=False)]
+    # print("숫자 아님 (seq 컬럼):")
+    # print(non_numeric_seq["seq"].unique())
+
+    if df[col].isnull().any():
+        raise ValueError(f"'{col}' 컬럼에 결측치가 있다")
+    
+    if col == "seq":
+        if not df[col].str.isnumeric().all():
+            raise ValueError(f"'{col}' 컬럼에 숫자가 아닌 값이 있다")
+        df[col] = df[col].astype(int)
+    else:
+        try:
+            df[col] = df[col].astype(float)
+        except ValueError:
+            raise ValueError(f"'{col}' 컬럼에 변환 불가능한 값이 있다다")
 
 # Parquet 저장
 parquet_path = "./output.parquet"
